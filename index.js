f51b451b514e59d07f4ecb86771791bff3ad0243
David McGrann <dpmcgrann@gmail.com>
2019-01-09 19:22:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks(distance) {
-  if (distance <= 42);
+  if (distance <= 42); {
     return distance - 42`;
-  
+  }else{
+    return 42 - distance'
+  }
 }
